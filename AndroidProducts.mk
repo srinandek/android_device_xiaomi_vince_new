8068c490c4e152d13c5146e5e5c1f5cab5afa6ba
srinandek <66463272+srinandek@users.noreply.github.com>
2020-06-16 10:52:42 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/p404_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    p404_vince-user \
+    p404_vince-userdebug \
+    p404_vince-eng
